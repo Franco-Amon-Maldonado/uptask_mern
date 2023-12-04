d0aaad4f099330cb46d092d6d49651ebe90dae5e
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-12-03 22:16:31 -0300

    03/11/2023
    -Primer commit
    -Configuracion acceso a data_base mongodb
    -Instalacion mongoose, nodemon, express
    -Inicio servidor de express
    -Configuracion puerto
    -Inicio de variables de entorno
    .Instalacion dotenv

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+
+import mongoose from 'mongoose'
+
+const connectionDB = async () => {
+	try {
+		const connection = await mongoose.connect(process.env.PASSWORD_MONGO)
+
+        const url = `${connection.connection.host}:${connection.connection.port}`
+        console.log(`Mongo conectadi en: ${url}`);
+	} catch (error) {
+		console.error(error.message)
+		process.exit(1) //Forza el proceso a terminar
+	}
+}
+
+export default connectionDB
---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv'
+
+import connectionDB from './config/db.js';
+
+const app = express()
+
+dotenv.config()
+
+connectionDB()
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`Puerto abierto en ${PORT}`);
+})
