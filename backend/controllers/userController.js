9ede3745a588d2f97a76b9e76226c5dc59f6adc0
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-12-10 22:47:22 -0300

    Add prevent users duplicate

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,16 @@ import User from "../models/User.js"
 
 
 const register = async (req, res) =>{
+    //Evitar registro duplicado
+
+    const { email} = req.body
+    const userExists = await User.findOne({ email: email})
+
+    if(userExists) {
+        const error = new Error ("User already exists")
+        return res.status(400).json({message: error.message})
+    }
+
 
     try {
         const user = new User(req.body)
